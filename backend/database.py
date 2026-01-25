212cc52d22f41b1952af8449e39d0812ee0a02a3
Piyushpal017 <piyushpal017@gmail.com>
2026-01-25 23:11:47 +0530

    add tables

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Base
 
 DATABASE_URL = "sqlite:///iot.db"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
+Base.metadata.create_all(bind=engine)
